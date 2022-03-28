502a722760f9e691e692d3821aa54f92ec747579
Tim Romanowski <timromanowski15@gmail.com>
2022-03-28 12:19:22 +0200

    Add data to app.js

---
+++ src/components/Recipe.stories.js
@@ -0,0 +1,12 @@
+import Recipe from './Recipe.js';
+
+export default {
+  title: 'components/Recipe',
+  component: Recipe,
+  argTypes: {},
+};
+
+const Template = args => <Recipe {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
