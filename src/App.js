77e3fbdfa2a69ab1e0704b3afee3345661530166
Tim Romanowski <timromanowski15@gmail.com>
2022-03-31 10:17:43 +0200

    update fetch from api on button click

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import RecipeList from './components/RecipeList';
 import { useState } from 'react';
 
 function App() {
-  const [recipeID, setRecipeID] = useState([]);
   const [recipes, setRecipes] = useState([]);
   const { REACT_APP_API_KEY } = process.env;
   let randomNumber1 = Math.floor(Math.random() * 5);
@@ -14,26 +13,17 @@ function App() {
   const urlIngredients = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${REACT_APP_API_KEY}&ingredients=${ingredients1[randomNumber1]},+${ingredients2[randomNumber2]}&number=1`;
 
   function handleRandomize() {
-    async function getNewRecipeID() {
+    async function getNewRecipe() {
       try {
         const response = await fetch(urlIngredients);
         const data = await response.json();
         const recipeID = data[0].id;
         console.log(recipeID);
-        setRecipeID(recipeID);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    getNewRecipeID();
-
-    async function getNewRecipe() {
-      try {
         let urlID = `https://api.spoonacular.com/recipes/${recipeID}/information?apiKey=${REACT_APP_API_KEY}`;
-        const response = await fetch(urlID);
-        const data = await response.json();
-        console.log(data);
-        setRecipes([...recipes, data]);
+        const response2 = await fetch(urlID);
+        const data2 = await response2.json();
+        console.log(data2);
+        setRecipes([...recipes, data2]);
       } catch (error) {
         console.log(error);
       }
@@ -45,7 +35,7 @@ function App() {
     <MainWrapper>
       <h1>Click the button to get a Random meal</h1>
       <Button text="Randomize" onRandomize={handleRandomize} />
-      {typeof recipeID === 'number' && <RecipeList recipes={recipes} />}
+      {recipes.length > 0 && <RecipeList recipes={recipes} />}
     </MainWrapper>
   );
 }
