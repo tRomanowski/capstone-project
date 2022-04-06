abff936ca825ded5ea8ed9350ef7bf7cd429bc71
Tim Romanowski <timromanowski15@gmail.com>
2022-04-06 12:35:04 +0200

    Add comments to sponnacular/index.mjs and creat folder backend

--- api/spoonacular/index.mjs
+++ api/spoonacular/index.mjs
@@ -5,6 +5,7 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { ingredient1, ingredient2, ingredient3, ingredient4, ingredient5 } =
       req.body;
+    // if form input is submitted with 5 ingredients then fetch this
     if (
       ingredient1 &&
       ingredient2 &&
@@ -16,6 +17,7 @@ export default async function handler(req, res) {
         `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${REACT_APP_API_KEY}&ingredients=${ingredient1},+${ingredient2},+${ingredient3},+${ingredient4},+${ingredient5}&number=1`
       );
       const predata = await responds.json();
+      // if fetch failed and response is an empty array then return template recipe
       if (predata.length === 0) {
         let data = {
           title: 'Sorry there was no recipe found for you!',
@@ -39,12 +41,13 @@ export default async function handler(req, res) {
       res.status(200).json(data);
       return;
     }
-
+    // if form input is submitted with 4 ingredients then fetch this
     if (ingredient1 && ingredient2 && ingredient3 && ingredient4) {
       const responds = await fetch(
         `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${REACT_APP_API_KEY}&ingredients=${ingredient1},+${ingredient2},+${ingredient3},+${ingredient4}&number=1`
       );
       const predata = await responds.json();
+      // if fetch failed and response is an empty array then return template recipe
       if (predata.length === 0) {
         let data = {
           title: 'Sorry there was no recipe found for you!',
@@ -69,12 +72,13 @@ export default async function handler(req, res) {
       res.status(200).json(data);
       return;
     }
-
+    // if form input is submitted with 3 ingredients then fetch this
     if (ingredient1 && ingredient2 && ingredient3) {
       const responds = await fetch(
         `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${REACT_APP_API_KEY}&ingredients=${ingredient1},+${ingredient2},+${ingredient3}&number=1`
       );
       const predata = await responds.json();
+      // if fetch failed and response is an empty array then return template recipe
       if (predata.length === 0) {
         let data = {
           title: 'Sorry there was no recipe found for you!',
@@ -99,12 +103,13 @@ export default async function handler(req, res) {
       res.status(200).json(data);
       return;
     }
-
+    // if form input is submitted with 2 ingredients then fetch this
     if (ingredient1 && ingredient2) {
       const responds = await fetch(
         `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${REACT_APP_API_KEY}&ingredients=${ingredient1},+${ingredient2}&number=1`
       );
       const predata = await responds.json();
+      // if fetch failed and response is an empty array then return template recipe
       if (predata.length === 0) {
         let data = {
           title: 'Sorry there was no recipe found for you!',
