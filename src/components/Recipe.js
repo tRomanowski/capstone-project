5b9e77e0f32a46982e707062c4cfcd9c4f9f9f6b
Tim Romanowski <timromanowski15@gmail.com>
2022-04-20 13:31:45 +0200

    Add Sidebar, update Header, update globalStyles

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -50,7 +50,7 @@ export default function Recipe({
 const RecipeCard = styled.section`
   border-radius: 20px;
   box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-  background-color: #65a603;
+  background-color: var(--primary-color);
   padding: 10px;
   margin: 20px 5px 0;
   display: grid;
